cb28781b11387bd6fb7152d8eb54218bc6c8d17a
shaksham08 <shakshamsinha@outlook.com>
2020-12-18 23:42:54 +0530

    Adding basic model

---
+++ models/campground.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongooses");
+
+//Defining The Campground Schema
+const CampgroundSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+});
+
+//Creating a Model From that Schema
+const Campground = mongoose.model("Campground", CampgroundSchema);
+
+module.exports = Campground;
