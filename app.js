ceae45172884388044a5ccbde9525bf4c728eace
shaksham08 <shakshamsinha@outlook.com>
2020-12-22 14:03:13 +0530

    Added campground show new and create

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-
-//Models
 const Campground = require("./models/campground");
+var methodOverride = require("method-override");
+
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -24,25 +24,41 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride("_method"));
 
+//Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campground/index", { campgrounds });
+  res.render("campgrounds/index", { campgrounds });
 });
 
 app.get("/campgrounds/new", (req, res) => {
-  res.render("campground/new");
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
 });
 
 app.post("/campgrounds", async (req, res) => {
-  const { title, description, location, price } = req.body;
-  const newCampground = new Campground({ title, description, location, price });
+  const { title, location } = req.body.campground;
+  const newCampground = new Campground({ title, location });
   await newCampground.save();
   res.redirect("/campgrounds");
+});
+
+app.patch("/campgrounds/:id", async (req, res) => {
+  res.send("PATCH REQUEST");
 });
 
 const PORT = 1234;
